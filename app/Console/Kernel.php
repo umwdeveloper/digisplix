d4213e2d5a7a4e962daf4f0d3193ed0f4197a318
uosdevelopers <uosdevelopers@gmail.com>
2023-12-29 13:18:04 +0500

    Queue worker fixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel {
         $schedule->command('app:invoice-status-command')->everyFiveMinutes();
 
         Log::info("Queue command running");
-        $schedule->command('queue:work --stop-when-empty')
+        $schedule->command('queue:work --max-time=120')
             ->everyMinute()
             ->withoutOverlapping();
     }
