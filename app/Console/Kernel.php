de48b020b4428074e8c9a69d4eaeae7cafb07db9
uosdevelopers <uosdevelopers@gmail.com>
2023-12-29 15:40:58 +0500

    Queue worker fixed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,8 +17,7 @@ class Kernel extends ConsoleKernel {
         Log::info("Queue command running");
         $schedule->command('queue:work --max-time=120')
             ->everySecond()
-            ->withoutOverlapping()
-            ->appendOutputTo(storage_path('logs/queue-work.log'));
+            ->withoutOverlapping();
     }
 
     /**
