f68aa1aeac8e62647f3ffe13b8d321b6f1865b73
uosdevelopers <uosdevelopers@gmail.com>
2024-01-02 11:28:02 +0500

    Bugs fixed

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'DigiSplix')
-                <img style="=width: 160px; height: auto;" src="https://admin.digisplix.com/images/Logo-Png.png"
+                <img style="width: 160px; height: auto;" src="https://admin.digisplix.com/images/Logo-Png.png"
                     class="logo">
             @else
                 {{ $slot }}
