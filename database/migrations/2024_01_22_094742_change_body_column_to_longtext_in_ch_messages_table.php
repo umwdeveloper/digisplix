6a12000a6263a6bedf4f686de8759d78b43643cc
uosdevelopers <uosdevelopers@gmail.com>
2024-01-22 14:51:51 +0500

    Bugs fixed

--- database/migrations/2024_01_22_094742_change_body_column_to_longtext_in_ch_messages_table.php
+++ database/migrations/2024_01_22_094742_change_body_column_to_longtext_in_ch_messages_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void {
         Schema::table('ch_messages', function (Blueprint $table) {
-            $table->longText('body')->change();
+            $table->longText('body')->nullable()->change();
         });
     }
 
