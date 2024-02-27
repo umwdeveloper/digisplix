c0d7e5908d7dabcb4ddf17d004d68534bf532758
uosdevelopers <uosdevelopers@gmail.com>
2024-02-27 17:40:21 +0500

    Bugs fixing

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -573,7 +573,7 @@ class PaymentController extends Controller {
 
         $items = array();
         Log::info("Items: ", $invoice->items->toArray());
-        $invoice->items->each(function ($item) use ($items) {
+        $invoice->items->each(function ($item) use (&$items) {
             array_push($items, [
                 'description' => $item->description,
                 'price' => $item->price,
